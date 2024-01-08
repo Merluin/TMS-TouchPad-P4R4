977ab43531236e8cdb9cc3397d22eba2de71b1fa
Thomas <thomas.quettier@gmail.com>
2024-01-08 11:22:18 +0100

    ENH

--- serial_led_AD/serial_led_AD.ino
+++ serial_led_AD/serial_led_AD.ino
@@ -1,18 +1,12 @@
-const int ledPin = 13; // Define the LED pin as 13
+const int ledPin = 7; // Define the LED pin as 13
 
 void setup() {
   pinMode(ledPin, OUTPUT); // Set the LED pin as an output
-  Serial.begin(9600);     // Initialize serial communication at 9600 baud
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    char receivedChar = Serial.read(); // Read the incoming character from serial
-
-    if (receivedChar == '1') {
-      digitalWrite(ledPin, HIGH); // Turn on the LED
-      delay(1000);               // Wait for 1 second
+      digitalWrite(ledPin, HIGH); 
+      delay(1);
       digitalWrite(ledPin, LOW);  // Turn off the LED
-    }
-  }
+      delay(1000);
 }
