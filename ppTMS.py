b2a40970ee32cfc33f617df86ac23211952ca6c9
Thomas <thomas.quettier@gmail.com>
2024-02-07 20:42:41 +0100

    Update ppTMS.py

--- ppTMS.py
+++ ppTMS.py
@@ -168,8 +168,12 @@ class SerialApp(QtWidgets.QMainWindow):
         spinBox.setRange(minValue, maxValue)
         spinBox.setValue(defaultValue)
         spinBox.setFixedHeight(60)
+         spinBox.setStyleSheet("""
+            QSpinBox::up-button   { width: 30px; height: 30px; }
+            QSpinBox::down-button { width: 30px; height: 30px; }
+        """)
         spinBox.setFont(font)
-        spinBox.valueChanged.connect(lambda value, name=spinBoxName: self.spinBoxValueChanged(value, name))
+        #spinBox.valueChanged.connect(lambda value, name=spinBoxName: self.spinBoxValueChanged(value, name))
         spinBoxLayout.addWidget(spinBox)
 
         valueLabel = QtWidgets.QLabel(str(defaultValue))
