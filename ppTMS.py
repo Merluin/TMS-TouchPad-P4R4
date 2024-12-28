88351851749ab36c0f9562e4980acda361615a99
Thomas <thomas.quettier@gmail.com>
2024-12-28 19:39:58 +0100

    ENH

--- ppTMS.py
+++ ppTMS.py
@@ -84,7 +84,7 @@ class SerialApp(QMainWindow):
             
         try:
             # Send the command to Arduino to request voltage
-            self.arduinoSerial.write(b'SET,test,9\n')    
+            self.arduinoSerial.write(b'9\n')    
         
             try:
                 # Wait for response
